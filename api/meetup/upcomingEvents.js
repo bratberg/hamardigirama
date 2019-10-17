60a64ac0a8823d9c5b319107fecd61012c3b8877
Espen Bratberg <bratberg@gmail.com>
2019-10-17 14:43:59 +0200

    Update upcomingEvents.js

--- api/meetup/upcomingEvents.js
+++ api/meetup/upcomingEvents.js
@@ -1,7 +1,6 @@
 const fetch = require('node-fetch');
 
 export default function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
     fetch('https://api.meetup.com/Hamar-Digirama/events?status=upcoming&desc=true')
         .then(r => r.json())
         .then(res.json)
